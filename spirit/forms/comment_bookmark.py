f47e97fb11835fb05bc7a85161bce5d495959c76
nitely <ochdownloader@gmail.com>
2014-05-12 19:29:51 -0300

    comment bookmark refactor

--- spirit/forms/comment_bookmark.py
+++ spirit/forms/comment_bookmark.py
@@ -10,3 +10,15 @@ class BookmarkForm(forms.ModelForm):
     class Meta:
         model = CommentBookmark
         fields = ['comment_number', ]
+
+    def __init__(self, user=None, topic=None, *args, **kwargs):
+        super(BookmarkForm, self).__init__(*args, **kwargs)
+        self.user = user
+        self.topic = topic
+
+    def save(self, commit=True):
+        comment_number = self.cleaned_data['comment_number']
+
+        # Bookmark is created/updated on topic view.
+        CommentBookmark.objects.filter(user=self.user, topic=self.topic)\
+            .update(comment_number=comment_number)
