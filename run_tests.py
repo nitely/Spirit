d435ce525eb9332d8ac2df2a575a486470cb4dd8
nitely <ochdownloader@gmail.com>
2014-11-20 15:16:49 -0300

    test runner, log warnings

--- run_tests.py
+++ run_tests.py
@@ -3,11 +3,18 @@ from __future__ import unicode_literals
 
 import os
 import sys
+import logging
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'example.settings_test_runner'
 
 import django
 from django.test.runner import DiscoverRunner
+
+
+def log_warnings():
+    logger = logging.getLogger('py.warnings')
+    handler = logging.StreamHandler()
+    logger.addHandler(handler)
 
 
 def run_tests():
@@ -18,4 +25,5 @@ def run_tests():
 
 if __name__ == "__main__":
     django.setup()
+    log_warnings()
     run_tests()
