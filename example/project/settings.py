9feec85bae25c2324e7538089658e718a8a58712
nitely <ochdownloader@gmail.com>
2014-11-27 18:37:26 -0300

    example settings

--- example/project/settings.py
+++ example/project/settings.py
@@ -13,7 +13,7 @@ from spirit.settings import *
 # You may override spirit settings below...
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(__file__)
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
