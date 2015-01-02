327af2f59551dd3b48d6557f57a8e534b5b15c24
nitely <ochdownloader@gmail.com>
2015-01-02 03:31:58 -0300

    refactor topic moderate

--- spirit/urls/topic_moderate.py
+++ spirit/urls/topic_moderate.py
@@ -4,24 +4,23 @@ from __future__ import unicode_literals
 
 from django.conf.urls import patterns, url
 
-from spirit.views.topic_moderate import TopicModerateDelete, TopicModerateLock, \
-    TopicModeratePin, TopicModerateGlobalPin
+from spirit.views.topic_moderate import TopicModerateDelete, TopicModerateUnDelete, \
+    TopicModerateLock, TopicModerateUnLock, TopicModeratePin, TopicModerateUnPin, \
+    TopicModerateGlobalPin, TopicModerateGlobalUnPin
 
 
 urlpatterns = patterns(
     "spirit.views.topic_moderate",
 
-    url(r'^delete/(?P<pk>\d+)/$', TopicModerateDelete.as_view(), kwargs={'value': True, }, name='topic-delete'),
-    url(r'^undelete/(?P<pk>\d+)/$', TopicModerateDelete.as_view(), kwargs={'value': False, }, name='topic-undelete'),
+    url(r'^delete/(?P<pk>\d+)/$', TopicModerateDelete.as_view(), name='topic-delete'),
+    url(r'^undelete/(?P<pk>\d+)/$', TopicModerateUnDelete.as_view(), name='topic-undelete'),
 
-    url(r'^lock/(?P<pk>\d+)/$', TopicModerateLock.as_view(), kwargs={'value': True, }, name='topic-lock'),
-    url(r'^unlock/(?P<pk>\d+)/$', TopicModerateLock.as_view(), kwargs={'value': False, }, name='topic-unlock'),
+    url(r'^lock/(?P<pk>\d+)/$', TopicModerateLock.as_view(), name='topic-lock'),
+    url(r'^unlock/(?P<pk>\d+)/$', TopicModerateUnLock.as_view(), name='topic-unlock'),
 
-    url(r'^pin/(?P<pk>\d+)/$', TopicModeratePin.as_view(), kwargs={'value': True, }, name='topic-pin'),
-    url(r'^unpin/(?P<pk>\d+)/$', TopicModeratePin.as_view(), kwargs={'value': False, }, name='topic-unpin'),
+    url(r'^pin/(?P<pk>\d+)/$', TopicModeratePin.as_view(), name='topic-pin'),
+    url(r'^unpin/(?P<pk>\d+)/$', TopicModerateUnPin.as_view(), name='topic-unpin'),
 
-    url(r'^globallypin/(?P<pk>\d+)/$', TopicModerateGlobalPin.as_view(), kwargs={'value': True, },
-        name='topic-global-pin'),
-    url(r'^ungloballypin/(?P<pk>\d+)/$', TopicModerateGlobalPin.as_view(), kwargs={'value': False, },
-        name='topic-global-unpin'),
+    url(r'^globallypin/(?P<pk>\d+)/$', TopicModerateGlobalPin.as_view(), name='topic-global-pin'),
+    url(r'^ungloballypin/(?P<pk>\d+)/$', TopicModerateGlobalUnPin.as_view(), name='topic-global-unpin'),
     )
--- spirit/views/topic_moderate.py
+++ spirit/views/topic_moderate.py
@@ -15,10 +15,15 @@ from spirit.signals.topic_moderate import topic_post_moderate
 
 class TopicModerateBase(View):
 
-    actions = None  # (do_action, undo_action)
+    action = None
+    field_name = None
+    to_value = None  # bool
 
-    def update(self, request, pk, value, not_value):
-        raise NotImplementedError()
+    def update(self, pk):
+        not_value = not self.to_value
+        return Topic.objects\
+            .filter(**{'pk': pk, self.field_name: not_value})\
+            .update(**{self.field_name: self.to_value, })
 
     def send_signal(self, user, action):
         topic_post_moderate.send(sender=self.topic.__class__, user=user,
@@ -26,54 +31,76 @@ class TopicModerateBase(View):
 
     def post(self, request, *args, **kwargs):
         pk = kwargs['pk']
-        value = kwargs['value']
-        not_value = not value
-        count = self.update(request, pk, value, not_value)
+        count = self.update(pk)
 
-        if count and self.actions is not None:
-            action = self.actions[0] if value else self.actions[1]
-            self.send_signal(request.user, action)
+        if count and self.action is not None:
+            self.send_signal(request.user, self.action)
 
         return redirect(request.POST.get('next', self.topic.get_absolute_url()))
 
     def get(self, request, *args, **kwargs):
         return render(request, 'spirit/topic/topic_moderate.html', {'topic': self.topic, })
 
+    def check_configuration(self):
+        assert self.field_name is not None, "You forgot to set field_name attribute"
+        assert self.to_value is not None, "You forgot to set to_value attribute"
+
     @method_decorator(moderator_required)
     def dispatch(self, *args, **kwargs):
+        self.check_configuration()
         self.topic = get_object_or_404(Topic, pk=kwargs['pk'])
         return super(TopicModerateBase, self).dispatch(*args, **kwargs)
 
 
 class TopicModerateDelete(TopicModerateBase):
 
-    def update(self, request, pk, value, not_value):
-        return Topic.objects.filter(pk=pk, is_removed=not_value)\
-            .update(is_removed=value)
+    field_name = 'is_removed'
+    to_value = True
+
+
+class TopicModerateUnDelete(TopicModerateBase):
+
+    field_name = 'is_removed'
+    to_value = False
 
 
 class TopicModerateLock(TopicModerateBase):
 
-    actions = (CLOSED, UNCLOSED)
+    action = CLOSED
+    field_name = 'is_closed'
+    to_value = True
 
-    def update(self, request, pk, value, not_value):
-        return Topic.objects.filter(pk=pk, is_closed=not_value)\
-            .update(is_closed=value)
+
+class TopicModerateUnLock(TopicModerateBase):
+
+    action = UNCLOSED
+    field_name = 'is_closed'
+    to_value = False
 
 
 class TopicModeratePin(TopicModerateBase):
 
-    actions = (PINNED, UNPINNED)
+    action = PINNED
+    field_name = 'is_pinned'
+    to_value = True
 
-    def update(self, request, pk, value, not_value):
-        return Topic.objects.filter(pk=pk, is_pinned=not_value)\
-            .update(is_pinned=value)
+
+class TopicModerateUnPin(TopicModerateBase):
+
+    action = UNPINNED
+    field_name = 'is_pinned'
+    to_value = False
 
 
 class TopicModerateGlobalPin(TopicModerateBase):
 
-    actions = (PINNED, UNPINNED)
+    action = PINNED
+    field_name = 'is_globally_pinned'
+    to_value = True
 
-    def update(self, request, pk, value, not_value):
-        return Topic.objects.filter(pk=pk, is_globally_pinned=not_value)\
-            .update(is_globally_pinned=value)
+
+class TopicModerateGlobalUnPin(TopicModerateBase):
+
+    action = UNPINNED
+    field_name = 'is_globally_pinned'
+    to_value = False
