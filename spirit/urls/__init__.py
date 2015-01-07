f88daa4b0c7e6d95f3ad3047ba1d191ac0d22b63
nitely <ochdownloader@gmail.com>
2015-01-07 01:19:45 -0300

    register config in urls fix

--- spirit/urls/__init__.py
+++ spirit/urls/__init__.py
@@ -4,6 +4,11 @@ from __future__ import unicode_literals
 
 from django.conf.urls import patterns, include, url
 
+import djconfig
+
+from spirit.forms.admin import BasicConfigForm
+
+djconfig.register(BasicConfigForm)
 
 urls = patterns('',
                 url(r'^$', 'spirit.views.topic.topic_active_list', name='index'),
