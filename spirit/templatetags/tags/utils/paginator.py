3e9178fbeeb76a4b0e96ff7102445d2b5c1822ec
nitely <ochdownloader@gmail.com>
2015-01-10 18:05:11 -0300

    refactor render_paginator

--- spirit/templatetags/tags/utils/paginator.py
+++ spirit/templatetags/tags/utils/paginator.py
@@ -9,7 +9,8 @@ from .. import register
 from spirit.utils.paginator import paginate, yt_paginate
 
 
-def _render_paginator(template, context, page, page_var, hashtag):
+@register.simple_tag(takes_context=True)
+def render_paginator(context, page, page_var='page', hashtag=''):
     query_dict = context["request"].GET.copy()
 
     try:
@@ -32,6 +33,11 @@ def _render_paginator(template, context, page, page_var, hashtag):
         "extra_query": extra_query
     }
 
+    if isinstance(page, Page):
+        template = "spirit/paginator/_paginator.html"
+    else:
+        template = "spirit/paginator/_yt_paginator.html"
+
     return render_to_string(template, context)
 
 
@@ -47,14 +53,3 @@ def paginator_autopaginate(context, object_list, per_page=15, page_var='page', p
     # TODO: remove
     page_number = page_number or context["request"].GET.get(page_var, 1)
     return paginate(object_list, per_page=per_page, page_number=page_number)
-
-
-@register.simple_tag(takes_context=True)
-def render_paginator(context, page, page_var='page', hashtag=''):
-    # TODO: test!
-    if isinstance(page, Page):
-        template = "spirit/paginator/_paginator.html"
-    else:
-        template = "spirit/paginator/_yt_paginator.html"
-
-    return _render_paginator(template, context, page, page_var, hashtag)
