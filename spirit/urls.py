12eaf72e4d4b997816c1bd3ac78b3e18843d80b1
nitely <ecastroborsani@gmail.com>
2015-05-02 20:28:42 -0300

    cleanup

--- spirit/urls.py
+++ spirit/urls.py
@@ -10,7 +10,10 @@ urls = patterns(
 
     url(r'^$', 'spirit.apps.topic.views.topic_active_list', name='index'),
     url(r'^st/admin/', include('spirit.apps.admin.urls')),
+    url(r'^user/', include('spirit.apps.user.urls')),
+    url(r'^search/', include('spirit.apps.search.urls')),
     url(r'^category/', include('spirit.apps.category.urls')),
+
     url(r'^topic/', include('spirit.apps.topic.urls')),
     url(r'^topic/moderate/', include('spirit.apps.topic.moderate.urls')),
     url(r'^topic/unread/', include('spirit.apps.topic.unread.urls')),
@@ -18,13 +21,12 @@ urls = patterns(
     url(r'^topic/favorite/', include('spirit.apps.topic.favorite.urls')),
     url(r'^topic/private/', include('spirit.apps.topic.private.urls')),
     url(r'^topic/poll/', include('spirit.apps.topic.poll.urls')),
+
     url(r'^comment/', include('spirit.apps.comment.urls')),
     url(r'^comment/bookmark/', include('spirit.apps.comment.bookmark.urls')),
     url(r'^comment/flag/', include('spirit.apps.comment.flag.urls')),
     url(r'^comment/history/', include('spirit.apps.comment.history.urls')),
     url(r'^comment/like/', include('spirit.apps.comment.like.urls')),
-    url(r'^user/', include('spirit.apps.user.urls')),
-    url(r'^search/', include('spirit.apps.search.urls')),
     )
 
 urlpatterns = patterns('', url(r'^', include(urls, namespace="spirit", app_name="spirit")))
