9f2c30e0427cdbfd6a418d61a74030bfd04362dc
nitely <ecastroborsani@gmail.com>
2015-05-25 16:17:08 -0300

    gravatar http to https, fix #49

--- spirit/utils/tags/gravatar.py
+++ spirit/utils/tags/gravatar.py
@@ -11,7 +11,7 @@ from spirit.templatetags.registry import register
 
 @register.simple_tag()
 def get_gravatar_url(user, size, rating='g', default='identicon'):
-    url = "http://www.gravatar.com/avatar/"
+    url = "https://www.gravatar.com/avatar/"
     hash = hashlib.md5(user.email.strip().lower().encode('utf-8')).hexdigest()
     data = urlencode([('d', urlquote(default)),
                       ('s', str(size)),
