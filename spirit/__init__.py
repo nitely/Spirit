169142f1e3aa3d1d8356d83d873bde0e1fdc8263
nitely <ecastroborsani@gmail.com>
2015-07-28 00:56:21 -0300

    bump 0.3.0

--- spirit/__init__.py
+++ spirit/__init__.py
@@ -4,6 +4,6 @@ from __future__ import unicode_literals
 
 # TODO: check django User.count >= spirit custom.User.count
 # in the migration to remove that table/model (Spirit v0.4)
-__version__ = '0.1.3'
+__version__ = '0.3.0'
 
 default_app_config = 'spirit.apps.SpiritConfig'
