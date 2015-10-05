10a3d407a11f52b024e912f2e833a34d27d812fd
nitely <ecastroborsani@gmail.com>
2015-10-04 22:53:13 -0300

    gulp coffescript

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,25 @@
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var sass = require('gulp-ruby-sass');
+var coffee = require('gulp-coffee');
+var sourcemaps = require('gulp-sourcemaps');
 
 var assetsPath = 'spirit/core/static/spirit/';
 var cssPath = assetsPath + 'stylesheets/';
+var jsPath = assetsPath + 'scripts/';
 
 
 gulp.task('sass', function () {
-  return sass(cssPath + 'src/styles.scss')
-    .on('error', sass.logError)
-    .pipe(gulp.dest(cssPath));
+    return sass(cssPath + 'src/styles.scss')
+        .on('error', sass.logError)
+        .pipe(gulp.dest(cssPath));
+});
+
+
+gulp.task('coffee', function() {
+    gulp.src(jsPath + '*.coffee')
+        .pipe(sourcemaps.init())
+        .pipe(coffee({bare: false}).on('error', gutil.log))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(jsPath))
 });
