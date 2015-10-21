d75c607110abec53aa109ab10090a2c17530c5f6
nitely <ecastroborsani@gmail.com>
2015-10-21 00:33:22 -0300

    core apps, fix #80

---
+++ spirit/core/apps.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class SpiritCoreConfig(AppConfig):
+
+    name = 'spirit.core'
+    verbose_name = "Spirit Core"
+    label = 'spirit_core'
