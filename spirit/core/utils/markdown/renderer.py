f7f35d6f7a5309a1a47476766fcf7d6ca65fb5cd
Ales Kocjancic <alesdotio@gmail.com>
2016-05-05 10:26:54 +0200

    removed unnecessary try except block in youtube embed

--- spirit/core/utils/markdown/renderer.py
+++ spirit/core/utils/markdown/renderer.py
@@ -98,13 +98,10 @@ class Renderer(mistune.Renderer):
 
     def youtube(self, video_id, start_minutes=None, start_seconds=None):
         timestamp = 0
-        try:
-            if start_minutes:
-                timestamp += int(start_minutes[:-1]) * 60
-            if start_seconds:
-                timestamp += int(start_seconds[:-1])
-        except ValueError:
-            pass
+        if start_minutes:
+            timestamp += int(start_minutes[:-1]) * 60
+        if start_seconds:
+            timestamp += int(start_seconds[:-1])
         timestamp = ('&start=%s' % timestamp) if timestamp else ''
         return '<span class="video"><iframe src="https://www.youtube.com/embed/{video_id}?html5=1{timestamp}" ' \
                'allowfullscreen></iframe></span>\n'.format(video_id=video_id, timestamp=timestamp)
