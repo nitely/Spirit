078309a477d6f41f63418fbfa212d42111aac824
nitely <ecastroborsani@gmail.com>
2016-11-14 14:48:14 -0300

    models import cleaning

--- spirit/user/models.py
+++ spirit/user/models.py
@@ -8,7 +8,6 @@ from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext_lazy as _
 from django.utils import timezone
 from django.conf import settings
-from django.contrib.auth.models import AbstractUser
 
 from ..core.utils.models import AutoSlugField
 
