7d54f3101d8a18de16dbe6b03e0c66b027c6bb90
grantal <AlexV348@gmail.com>
2017-06-09 11:22:41 -0700

    made a filter field

---
+++ spirit/topic/static/select_course_no.js
@@ -0,0 +1,9 @@
+// This is a function that will be used by spirit/topic/_top_bar.html
+// When someone clicks 
+
+
+function go_to_course_number() {
+  var no = document.getElementById("course_no").value
+  alert(no)
+
+}
