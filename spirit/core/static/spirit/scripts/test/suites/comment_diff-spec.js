f74cd1c176f057f482ad39a01a511b6da4475464
nitely <ecastroborsani@gmail.com>
2017-10-06 13:07:14 -0300

    remove jquery from comment_diff

---
+++ spirit/core/static/spirit/scripts/test/suites/comment_diff-spec.js
@@ -0,0 +1,21 @@
+(function() {
+  describe("comment_diff plugin tests", function() {
+    beforeEach(function() {
+      var fixtures;
+      fixtures = jasmine.getFixtures();
+      fixtures.fixturesPath = 'base/test/fixtures/';
+      return loadFixtures('comment_diff.html');
+    });
+    return it("diffes comments", function() {
+      var comments;
+      comments = document.querySelectorAll('.comment');
+      stModules.commentDiff(comments);
+      expect(comments[0].innerHTML).toEqual('Hello');
+      expect(comments[1].innerHTML).toEqual('Hello<ins> world</ins>');
+      return expect(comments[2].innerHTML).toEqual('Hello world<ins>!</ins>');
+    });
+  });
+
+}).call(this);
+
+//# sourceMappingURL=comment_diff-spec.js.map
