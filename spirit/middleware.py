55464650c76a9039ecb03245a9e9d415ce416f07
nitely <ecastroborsani@gmail.com>
2018-09-02 20:08:08 -0300

    deprecate spirit.middleware properly

--- spirit/middleware.py
+++ spirit/middleware.py
@@ -3,8 +3,15 @@
 from spirit.core.middleware import XForwardedForMiddleware, PrivateForumMiddleware
 from spirit.user.middleware import TimezoneMiddleware, LastIPMiddleware,\
     LastSeenMiddleware, ActiveUserMiddleware
+from .core.utils.deprecations import warn
 
-# TODO: remove in Spirit 0.5
+warn('This will get removed in Spirit 0.7. '
+     'Use spirit.core.middleware.XForwardedForMiddleware, '
+     'spirit.core.middleware.PrivateForumMiddleware, '
+     'spirit.user.middleware.TimezoneMiddleware, '
+     'spirit.user.middleware.LastIPMiddleware, '
+     'spirit.user.middleware.LastSeenMiddleware and '
+     'spirit.user.middleware.ActiveUserMiddleware instead')
 
 
 __all__ = [
