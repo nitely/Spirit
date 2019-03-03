f4b6f94b61cf8ec3993db0d0bcd7a6f10e4bc6e4
nitely <ecastroborsani@gmail.com>
2019-03-02 21:45:15 -0300

    capture python warnings

--- spirit/extra/project_template/project_name/wsgi.py
+++ spirit/extra/project_template/project_name/wsgi.py
@@ -2,12 +2,13 @@
 
 from __future__ import absolute_import, unicode_literals
 
+import logging
 import os
 
 from django.core.wsgi import get_wsgi_application
 
 
-os.environ.setdefault("PYTHONWARNINGS", "default")
+logging.captureWarnings(True)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings.dev")
 
 application = get_wsgi_application()
